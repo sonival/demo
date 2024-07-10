ea3500b7ad7595da6f4028fc56a424c63924f92c
Soni <sonival.santos@gmail.com>
2024-07-09 22:15:41 -0300

    configure database

---
+++ java/demoJWT/src/main/java/user/User.java
@@ -0,0 +1,29 @@
+package user;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firtname;
+    private String lastname;
+    private String email;
+    private String password;
+
+}

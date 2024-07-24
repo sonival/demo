e8bf12327457ec46ccb6504d870f9dfb68ac0427
Soni <sonival.santos@gmail.com>
2024-07-24 16:02:39 -0300

    startup project

---
+++ react-playground/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import img1 from '../assets/react.svg'
+export default function Home() {
+  return (
+    <>
+   <>Home</>
+   <Image src={img1} alt="Demo IMG"></Image>
+  
+    </>
+  );
+}
